cce4b72e5e07a2334b224d9b1007cd3e47a95a7a
Leon <47238971+pxzzo@users.noreply.github.com>
2023-08-10 19:40:46 +0200

    Update RealFunk.user.js

--- RealFunk.user.js
+++ RealFunk.user.js
@@ -11,8 +11,8 @@
 
 socket.on("vehicleFMS", (vehicleFMSObject) =>{
 
- createRadioMessage(vehicleFMSObject);
- console.log(`
+    createRadioMessage(vehicleFMSObject);
+    console.log(`
     ${vehicleFMSObject.userVehicleName}
     changed Status to ${vehicleFMSObject.userVehicleFMS}
     (ID: ${vehicleFMSObject.userVehicleID})
@@ -23,8 +23,14 @@ socket.on("vehicleFMS", (vehicleFMSObject) =>{
         type : "GET",
         id : vehicleFMSObject.userDepartmentID,
     }).then((department) => {
-    new Audio(`http://api.voicerss.org/?key=bb4962bbfef84781841b9824d5ad6137&hl=de-de&v=Lina&f=16khz_16bit_stereo&src=${department[0].userBuildingName}%20für%20${vehicleFMSObject.userVehicleName},%20wir%20gehen%20in%20den%20Status%20${vehicleFMSObject.userVehicleFMS}`).play()
-    });
+        new Audio(`http://api.voicerss.org/?key=bb4962bbfef84781841b9824d5ad6137&hl=de-de&v=Lina&f=16khz_16bit_stereo&src=${department[0].userBuildingName}%20für%20${vehicleFMSObject.userVehicleName}!`).play()
+        setTimeout(() => {
+            new Audio(`http://api.voicerss.org/?key=bb4962bbfef84781841b9824d5ad6137&hl=de-de&v=Jonas&f=16khz_16bit_stereo&src=${department[0].userBuildingName} hört!`).play()
+        }, 8000)
+        setTimeout(() => {
+            new Audio(`http://api.voicerss.org/?key=bb4962bbfef84781841b9824d5ad6137&hl=de-de&v=Lina&f=16khz_16bit_stereo&src=Wir gehen in den Status ${vehicleFMSObject.userVehicleFMS}!`).play()
+        }, 13000)
+    })
 
- //Change Status in department List
+    //Change Status in department List
 });
