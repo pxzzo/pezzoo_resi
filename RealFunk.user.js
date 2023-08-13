6a69c98f471ebca899f3e3a74ddf5d2c4a42e4e6
Leon <47238971+pxzzo@users.noreply.github.com>
2023-08-14 01:01:04 +0200

    Update RealFunk.user.js

--- RealFunk.user.js
+++ RealFunk.user.js
@@ -8,42 +8,46 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=rettungssimulator.online
 // @grant        none
 // ==/UserScript==
-const apiKey = "YOUR_API_KEY"
+const apiKey = "YOUR_API_KEY";
 
-
-socket.on("vehicleFMS", (vehicleFMSObject) =>{
-
- createRadioMessage(vehicleFMSObject);
- console.log(`
+socket.on("vehicleFMS", (vehicleFMSObject) => {
+  createRadioMessage(vehicleFMSObject);
+  console.log(`
     ${vehicleFMSObject.userVehicleName}
     changed Status to ${vehicleFMSObject.userVehicleFMS}
     (ID: ${vehicleFMSObject.userVehicleID})
     and DepartmentID
  `);
- const departmentID = vehicleFMSObject.userDepartmentID
-    $.ajax({
-        url: "/api/userBuildings",
-        dataType: "json",
-        type : "GET"
-    }).then((department) => {
-        function findBuildingByID(dpID) {
-    return department.find(building => building.userBuildingID === dpID);
-}
+  const departmentID = vehicleFMSObject.userDepartmentID;
+  $.ajax({
+    url: "/api/userBuildings",
+    dataType: "json",
+    type: "GET",
+  }).then((department) => {
+    function findBuildingByID(dpID) {
+      return department.find((building) => building.userBuildingID === dpID);
+    }
 
-const foundBuilding = findBuildingByID(departmentID);
+    const foundBuilding = findBuildingByID(departmentID);
 
-if (foundBuilding) {
-    console.log("Found building:", foundBuilding);
-    new Audio(`http://api.voicerss.org/?key=${apiKey}&hl=de-de&v=Lina&f=16khz_16bit_stereo&src=${foundBuilding.userBuildingName}%20für%20${vehicleFMSObject.userVehicleName}!`).play()
-        setTimeout(() => {
-        new Audio(`http://api.voicerss.org/?key=${apiKey}&hl=de-de&v=Jonas&f=16khz_16bit_stereo&src=${foundBuilding.userBuildingName} hört!`).play()
-        }, 8000)
-        setTimeout(() => {
-        new Audio(`http://api.voicerss.org/?key=${apiKey}&hl=de-de&v=Lina&f=16khz_16bit_stereo&src=Wir gehen in den Status ${vehicleFMSObject.userVehicleFMS}!`).play()
-        }, 13000)
-} else {
-    console.log("Building not found.");
-    return
-}
-    
-    })
+    if (foundBuilding) {
+      console.log("Found building:", foundBuilding);
+      new Audio(
+        `http://api.voicerss.org/?key=${apiKey}&hl=de-de&v=Lina&f=16khz_16bit_stereo&src=${foundBuilding.userBuildingName}%20für%20${vehicleFMSObject.userVehicleName}!`
+      ).play();
+      setTimeout(() => {
+        new Audio(
+          `http://api.voicerss.org/?key=${apiKey}&hl=de-de&v=Jonas&f=16khz_16bit_stereo&src=${foundBuilding.userBuildingName} hört!`
+        ).play();
+      }, 8000);
+      setTimeout(() => {
+        new Audio(
+          `http://api.voicerss.org/?key=${apiKey}&hl=de-de&v=Lina&f=16khz_16bit_stereo&src=Wir gehen in den Status ${vehicleFMSObject.userVehicleFMS}!`
+        ).play();
+      }, 13000);
+    } else {
+      console.log("Building not found.");
+      return;
+    }
+  });
+});
