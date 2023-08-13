8b1865f1fb4c6b4ee5db3f82bee68f919499e4b1
Leon <47238971+pxzzo@users.noreply.github.com>
2023-08-14 01:23:11 +0200

    Update RealFunk.user.js

--- RealFunk.user.js
+++ RealFunk.user.js
@@ -10,23 +10,37 @@
 // ==/UserScript==
 const apiKey = "YOUR_API_KEY";
 
+let timeOut = 0;
+
 socket.on("vehicleFMS", (vehicleFMSObject) => {
+  if (timeOut === 1) {
+    console.log("Wird bereits ausgeführt, warte auf Beendigung.");
+    return;
+  } else {
+    doThis(vehicleFMSObject);
+  }
+});
+
+function doThis(vehicleFMSObject) {
+  let timeOut = 1;
   createRadioMessage(vehicleFMSObject);
   console.log(`
-    ${vehicleFMSObject.userVehicleName}
-    changed Status to ${vehicleFMSObject.userVehicleFMS}
-    (ID: ${vehicleFMSObject.userVehicleID})
-    and DepartmentID
- `);
+      ${vehicleFMSObject.userVehicleName}
+      changed Status to ${vehicleFMSObject.userVehicleFMS}
+      (ID: ${vehicleFMSObject.userVehicleID})
+      and DepartmentID
+   `);
   const departmentID = vehicleFMSObject.userDepartmentID;
   $.ajax({
     url: "/api/userBuildings",
     dataType: "json",
     type: "GET",
   }).then((department) => {
-    const foundBuilding = department.find(
-      (building) => building.userBuildingID === findBuildingByID(departmentID)
-    );
+    function findBuildingByID(dpID) {
+      return department.find((building) => building.userBuildingID === dpID);
+    }
+
+    const foundBuilding = findBuildingByID(departmentID);
 
     if (foundBuilding) {
       console.log("Found building:", foundBuilding);
@@ -37,15 +51,16 @@ socket.on("vehicleFMS", (vehicleFMSObject) => {
         new Audio(
           `http://api.voicerss.org/?key=${apiKey}&hl=de-de&v=Jonas&f=16khz_16bit_stereo&src=${foundBuilding.userBuildingName} hört!`
         ).play();
-      }, 8000);
+      }, 6000);
       setTimeout(() => {
         new Audio(
           `http://api.voicerss.org/?key=${apiKey}&hl=de-de&v=Lina&f=16khz_16bit_stereo&src=Wir gehen in den Status ${vehicleFMSObject.userVehicleFMS}!`
         ).play();
-      }, 13000);
+        return (timeOut = 0);
+      }, 10000);
     } else {
       console.log("Building not found.");
       return;
     }
   });
-});
+}
